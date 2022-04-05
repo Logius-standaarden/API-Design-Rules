ea33b9ac95e077347f070d2df0fb8b34f1b07334
Alexander Green <alexander.green@logius.nl>
2022-04-05 16:50:49 +0200

    Tables

--- js/config.js
+++ js/config.js
@@ -79,15 +79,6 @@ var respecConfig = {
   // this configuration can be set to make sure that the alle H1 headers and navigation bars are preserved
   nl_markdownSplitH1sections: false,
 
-  // in some cases the Repository of a Specification document and the Issue- and PR bases are split
-  // thi scan be configured below
-  // if not set, the url's will be generated on the respecConfig.github automatically  
-  nl_github: {
-    //    issueBase: "https://github.com/Logius-standaarden/Digikoppeling-Wat-is-Digikoppeling/issues",
-    revision: "https://github.com/Logius-standaarden/Digikoppeling-Architectuur",
-    //  pullrequests: "https://github.com/Logius-standaarden/Digikoppeling-Wat-is-Digikoppeling/pulls"
-  },
-
   // nl_organisationName is used for some company specific values in the header (and Sotd)
   // currently supported: Logius and Geonovum (default)  
   nl_organisationName: "Logius",
@@ -95,11 +86,6 @@ var respecConfig = {
   // prefix for the names of company specific css, svg and ico prefixes
   // defaults to "GN-"  
   nl_organisationPrefix: "LS-",
-
-  // class style can be automatically insertd in generated markdown tables
-  // currently defaults to simple, but this may change
-  // options simple, data, <your css>    
-  nl_markdownTableClass: "dkkvs",
 
   // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
   // so that figures can be linked are be a part of table of figures
