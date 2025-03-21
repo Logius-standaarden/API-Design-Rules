20b0ac272ebc5c3e27d99080156bd5c8e1d201fd
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-03-21 16:10:05 +0000

    Beheer: voeg baseline testcase toe
    Deze kunnen we makkelijker kopieren zodat in ieder geval
    alle minimale vereisten in de API zitten. Daarmee kan de
    testcase daadwerkelijk alles afvangen wat er uniek aan
    is.

--- linter/run-linter-tests.mjs
+++ linter/run-linter-tests.mjs
@@ -2,16 +2,18 @@ import {exec} from 'child_process';
 import utils from 'util';
 import * as path from 'path';
 import * as fs from 'fs';
+import { argv } from 'process';
 
 const __dirname = import.meta.dirname;
 const execute = utils.promisify(exec);
 const readFile = utils.promisify(fs.readFile);
 const readdir = utils.promisify(fs.readdir);
+const writeFile = utils.promisify(fs.writeFile);
 
 const SPECTRAL_RULESET_LOCATION = path.join(__dirname, 'spectral.yml');
 
 function computeTestCommand(apiLocation) {
-    return `spectral lint -r ${SPECTRAL_RULESET_LOCATION} ${apiLocation}/openapi.json`
+    return `spectral lint -r ${SPECTRAL_RULESET_LOCATION} ${apiLocation}/openapi.json || true`
 }
 
 function removeProcessDir(output) {
@@ -41,8 +43,12 @@ async function runCommand(apiLocation) {
     }
 }
 
+function expectedOutputLocation(apiLocation) {
+    return path.join(apiLocation, 'expected-output.txt');
+}
+
 async function readExpectedOutput(apiLocation) {
-    return removeProcessDir(await readFile(path.join(apiLocation, 'expected-output.txt'), {encoding: 'utf-8'}));
+    return removeProcessDir(await readFile(expectedOutputLocation(apiLocation), {encoding: 'utf-8'}));
 }
 
 async function obtainAllTestcases() {
@@ -50,9 +56,19 @@ async function obtainAllTestcases() {
     return [...apiDirectories.map(dir => path.join(dir.parentPath, dir.name))];
 }
 
+const shouldRefreshOutput = argv.includes('--refresh');
+
 for (const apiLocation of await obtainAllTestcases()) {
-    console.log(`Validating testcase ${apiLocation}`);
     const actualOutput = await runCommand(apiLocation);
+
+    if (shouldRefreshOutput) {
+        console.log(`Updated expected output of ${apiLocation}`);
+        await writeFile(expectedOutputLocation(apiLocation), actualOutput, {encoding: 'utf-8'});
+
+        continue;
+    }
+
+    console.log(`Validated expected output of ${apiLocation}`);
     const expectedOutput = await readExpectedOutput(apiLocation);
 
     if (actualOutput !== expectedOutput) {
