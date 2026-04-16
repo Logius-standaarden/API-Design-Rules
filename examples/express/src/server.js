f727644df3ed0d80db4f44fe2729c978a644a0df
Tim van der Lippe <Tim.Lippe@logius.nl>
2026-04-16 16:46:24 +0200

    Start met Node ExpressJS implementatie

--- examples/express/src/server.js
+++ examples/express/src/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const openapi = require('@wesleytodd/openapi');
+// @ts-check
+
+import express from 'express';
+import openapi from '@wesleytodd/openapi';
+
 const app = express();
 const port = 8080;
 
@@ -12,6 +15,79 @@ const API_VERSION_HEADER_SCHEMA = {
     type: 'string'
   }
 };
+const BAD_REQUEST_RESPONSE = {
+  'application/problem+json': {
+    schema: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'integer'
+        },
+        title: {
+          type: 'string'
+        },
+        detail: {
+          type: 'string'
+        },
+        errors: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              in: {
+                type: 'string'
+              },
+              location: {
+                type: 'string'
+              },
+              code: {
+                type: 'string'
+              },
+              detail: {
+                type: 'string'
+              },
+              index: {
+                type: 'integer'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+/**
+ * @typedef {{ in: "body"|"query", detail: string, location?: string, index?: number, code?: string }} BadRequestError
+ */
+
+class BadRequestResponseWithErrors extends Error {
+  /**
+   * @param {string} title
+   * @param {string} detail
+   * @param {Array<BadRequestError>} errors
+   */
+  constructor(title, detail, errors) {
+    super();
+
+    this.title = title;
+    this.detail = detail;
+    this.errors = errors;
+  }
+  toJSON() {
+    const {
+      title,
+      detail,
+      errors
+    } = this;
+    return {
+      status: 400,
+      title,
+      detail,
+      errors,
+    };
+  }
+}
 
 const oapi = openapi({
   openapi: '3.0.0',
@@ -87,6 +163,78 @@ app.get('/', oapi.path({
   }
 }), (req, res) => {
   res.send('Dit is de landing pagina van deze API');
+});
+
+app.post('/input', oapi.path({
+  description: 'Input method',
+  tags: [
+    'input'
+  ],
+  operationId: 'postInput',
+  responses: {
+    200: {
+      description: 'Input',
+      content: {
+        'text/plain': {}
+      },
+      headers: {
+        [API_VERSION_HEADER_NAME]: oapi.headers(API_VERSION_HEADER_SCHEMA_NAME)
+      }
+    },
+    400: {
+      description: 'Invalid input',
+      content: BAD_REQUEST_RESPONSE,
+      headers: {
+        [API_VERSION_HEADER_NAME]: oapi.headers(API_VERSION_HEADER_SCHEMA_NAME)
+      }
+    }
+  }
+}), (req, res) => {
+  /**
+   * @type {Array<BadRequestError>}
+   */
+  const errors = [];
+  const queryInput = req.query.queryInput;
+  if (!queryInput) {
+    errors.push({
+      in: 'query',
+      detail: 'Missing query parameter',
+      location: 'queryInput',
+      code: 'input.query.missing',
+    })
+  } else {
+    let params;
+    if (typeof queryInput === 'string') {
+      params = [queryInput];
+    } else {
+      params = queryInput;
+    }
+    for (const [index, param] of params.entries()) {
+      if (param !== '42') {
+        errors.push({
+          in: 'query',
+          detail: 'Invalid query parameter. Expected 42, but was ' + param,
+          location: 'queryInput',
+          index,
+          code: 'input.query.invalid',
+        });
+      }
+    }
+  }
+
+  if (errors.length > 0) {
+    throw new BadRequestResponseWithErrors('title', 'detail', errors);
+  }
+
+  res.send('Succesvolle response');
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof BadRequestResponseWithErrors) {
+    return res.status(400).json(err.toJSON());
+  }
+
+  next();
 });
 
 app.listen(port, () => {
